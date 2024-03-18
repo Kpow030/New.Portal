a91818b59f4821c2c20040af40fe2685debdc2d4
Aleksandr Titov <149224531+Kpow030@users.noreply.github.com>
2024-03-18 21:18:42 +0800

    models.py

--- News1/Newsportal/simpleapp/models.py
+++ News1/Newsportal/simpleapp/models.py
@@ -1,12 +1,25 @@
 from django.db import models
 from django.shortcuts import  reverse
 
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+
+
 class Post(models.Model):
     title = models.CharField(
         max_length=100)
-    Text = models.TextField()
+    Text = models.TextField(null=False)
     date_pub = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=128, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
 
     def get_absolute_url(self):
@@ -16,11 +29,11 @@ class Post(models.Model):
     def __str__(self):
         return '{}'.format(self.title)
 
-class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
 
 
 
 # Create your models here.
+
+
 
 
