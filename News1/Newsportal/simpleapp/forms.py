0cb8e7c68810cd3155f1ad43a428f69e4e698440
Aleksandr Titov <149224531+Kpow030@users.noreply.github.com>
2024-03-18 21:17:52 +0800

    forms.py

--- News1/Newsportal/simpleapp/forms.py
+++ News1/Newsportal/simpleapp/forms.py
@@ -1,11 +1,16 @@
 from django import forms
-from django.core.exceptions import ValidationError
-
-
-from .models import Post
 
 
 
+from .models import Post, Category
 
-#    class Meta:
-  #
+
+class PostSearchForm(forms.Form):
+    q = forms.CharField(required=False, label='Поиск по названию.')
+    category = forms.ModelChoiceField(queryset=Category.objects.all(), required=False, label='Поиск по категории.')
+    date_after = forms.DateField(input_formats=[{'format': '%Y-%m-%d'}], required=False, label='Поиск по дате.')
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'Text']
