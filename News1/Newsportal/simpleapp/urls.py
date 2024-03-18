ed21e5ce1e0b2a40199cb0e5c92353d8ae084cec
Aleksandr Titov <149224531+Kpow030@users.noreply.github.com>
2024-03-18 21:19:32 +0800

    urls.py

--- News1/Newsportal/simpleapp/urls.py
+++ News1/Newsportal/simpleapp/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
-#from .views import (PostList, PostDetail, PostCreate, PostUpdate, PostDelete)
-from .views import index, detail
+from django.urls import path, re_path
+from .views import index, detail, PostCreateView, PostUpdateView, PostDeleteView
+
 
 urlpatterns = [
 
     path('news_list/', index, name='index'),
     path('new/<str:slug>', detail, name='detail'),
+    path('create/', PostCreateView.as_view(), name='post_create'),
+    re_path(r'^(?P<pk>\d+)/edit/$', PostUpdateView.as_view(), name='post_edit'),
+    re_path(r'^(?P<pk>\d+)/delete/$', PostDeleteView.as_view(), name='post_delete'),
 ]
