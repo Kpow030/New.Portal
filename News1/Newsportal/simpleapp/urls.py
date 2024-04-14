d84f25b5d941c88005162ab77b16776556eb79c4
Aleksandr Titov <149224531+Kpow030@users.noreply.github.com>
2024-04-14 18:06:59 +0800

    urls.py

--- News1/Newsportal/simpleapp/urls.py
+++ News1/Newsportal/simpleapp/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, re_path
 from .views import index, detail, PostCreateView, PostUpdateView, PostDeleteView, CategoryList, subscribe
+from django.views.decorators.cache import cache_page
 
 
 
 urlpatterns = [
-    path('news_list/', index, name='index'),
-    path('new/<str:slug>', detail, name='detail'),
-    path('create/', PostCreateView.as_view(), name='post_create'),
-    re_path(r'^(?P<pk>\d+)/edit/$', PostUpdateView.as_view(), name='post_edit'),
-    re_path(r'^(?P<pk>\d+)/delete/$', PostDeleteView.as_view(), name='post_delete'),
-    path('categories/<int:pk>', CategoryList.as_view(), name = 'category_list'),
+    path('news_list/', cache_page(60*10), index, name='index'),
+    path('new/<str:slug>', cache_page(300*10), detail, name='detail'),
+    path('create/', cache_page(300*10), PostCreateView.as_view(), name='post_create'),
+    re_path(r'^(?P<pk>\d+)/edit/$', cache_page(300*10), PostUpdateView.as_view(), name='post_edit'),
+    re_path(r'^(?P<pk>\d+)/delete/$', cache_page(300*10), PostDeleteView.as_view(), name='post_delete'),
+    path('categories/<int:pk>', cache_page(300*10), CategoryList.as_view(), name = 'category_list'),
     path('categories/<int:pk>/subscribe', subscribe, name='suscribe'),
-
 ]
