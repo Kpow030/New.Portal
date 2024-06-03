20a6abaa404a73062b04516dbe0f2461a1b600ef
Aleksandr Titov <149224531+Kpow030@users.noreply.github.com>
2024-06-04 07:50:26 +0800

    models.py

--- News1/Newsportal/simpleapp/models.py
+++ News1/Newsportal/simpleapp/models.py
@@ -1,16 +1,17 @@
+from django.contrib.auth.models import User
+from django.core.cache import cache
 from django.db import models
-from django.shortcuts import  reverse
+from django.shortcuts import reverse
 
-
+from Newsportal.models.models import Author
 
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
+    subscribers = models.ManyToManyField(User, related_name='categories')
 
     def __str__(self):
         return self.name
-
-
 
 
 class Post(models.Model):
@@ -21,13 +22,33 @@ class Post(models.Model):
     slug = models.SlugField(max_length=128, unique=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
-
     def get_absolute_url(self):
         return reverse('detail', kwargs={'slug': self.slug})
 
-
     def __str__(self):
         return '{}'.format(self.title)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        cache.delete(f'product-{self.pk}')
+
+
+class Message(models.Model): # Модель отправки сообщений
+    text = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    sender = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='sent_message')
+    recipient = models.ForeignKey(Author, on_delete=models.CASCADE, related_name='received_message')
+
+    def __str__(self):
+        return f'{self.sender.name} to {self.recipient.name}: {self.text[:50]}'
+
+
+class File(models.Model): # Модель добавления в сообщения фалов
+    file = models.FileField(upload_to='files/')
+    message = models.ForeignKey(Message, on_delete=models.CASCADE, related_name='files')
+
+    def __str__(self):
+        return f'{self.file.name} in {self.message.text[:50]}'
 
 
 
